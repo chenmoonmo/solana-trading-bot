f9c4063b52930779b9d37c26c63bc08c7e7a5641
chenmoonmo <chenmoonmo@gmail.com>
2024-06-28 18:02:42 +0800

    feat: add test

--- test-2.ts
+++ test-2.ts
@@ -1,31 +1,101 @@
 import Client, { CommitmentLevel, SubscribeRequest } from '@triton-one/yellowstone-grpc';
-import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import {
+  createAssociatedTokenAccountIdempotentInstruction,
+  createAssociatedTokenAccountInstruction,
+  createMint,
+  createTransferInstruction,
+  getAssociatedTokenAddress,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+  TOKEN_PROGRAM_ID,
+} from '@solana/spl-token';
 import {
   LIQUIDITY_STATE_LAYOUT_V4,
   MAINNET_PROGRAM_ID,
+  DEVNET_PROGRAM_ID,
   MARKET_STATE_LAYOUT_V3,
   Token,
   publicKey,
 } from '@raydium-io/raydium-sdk';
 import bs58 from 'bs58';
-import { Connection, PublicKey } from '@solana/web3.js';
+import {
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  TransactionMessage,
+  VersionedTransaction,
+} from '@solana/web3.js';
 import { COMMITMENT_LEVEL, RPC_ENDPOINT, RPC_WEBSOCKET_ENDPOINT } from './helpers';
 
 (async function main() {
   const connection = new Connection(RPC_ENDPOINT, {
-    wsEndpoint: RPC_WEBSOCKET_ENDPOINT,
+    // wsEndpoint: RPC_WEBSOCKET_ENDPOINT,
     commitment: COMMITMENT_LEVEL,
   });
 
-  let pair = new PublicKey(bs58.decode('24EY192uRz52WAD3XtnrrmKfhSmucojKRd9TeRhfCL9c'));
-  let pairAccount = await connection.getAccountInfo(pair);
-  
-  let poolState = LIQUIDITY_STATE_LAYOUT_V4.decode(pairAccount!.data);
-  const poolOpenTime = parseInt(poolState.poolOpenTime.toString());
+  const fromWallet = Keypair.fromSecretKey(
+    Buffer.from([
+      118, 115, 173, 31, 167, 164, 242, 27, 166, 207, 55, 178, 53, 100, 176, 148, 151, 19, 221, 81, 62, 234, 180, 69,
+      145, 75, 87, 224, 109, 204, 152, 227, 13, 219, 36, 193, 2, 94, 124, 16, 188, 68, 97, 158, 13, 96, 191, 216, 77,
+      16, 119, 188, 238, 87, 134, 136, 88, 211, 15, 96, 33, 170, 3, 107,
+    ]),
+  );
 
-  console.log(poolOpenTime,poolState);
-  
+  const toWallet = Keypair.generate();
 
+  const mint = new PublicKey('AmY274sN59rwT6SKk6yeX4cS3w1AKzwHQrinToyyyfP5');
 
+  const fromTokenAccount = await getOrCreateAssociatedTokenAccount(connection, fromWallet, mint, fromWallet.publicKey);
 
+  const toTokenAccount = await getAssociatedTokenAddress(mint, toWallet.publicKey);
+
+  const instruction = createTransferInstruction(fromTokenAccount.address, toTokenAccount, fromWallet.publicKey, 1);
+
+  let transactions = [];
+
+  const latestBlockhash = await connection.getLatestBlockhash(COMMITMENT_LEVEL);
+
+  console.log(latestBlockhash);
+
+  for (let i = 0; i < 10; i++) {
+    const messageV0 = new TransactionMessage({
+      payerKey: fromWallet.publicKey,
+      recentBlockhash: latestBlockhash.blockhash,
+      instructions: [
+        createAssociatedTokenAccountIdempotentInstruction(
+          fromWallet.publicKey,
+          toTokenAccount,
+          toWallet.publicKey,
+          mint,
+        ),
+        instruction,
+      ],
+    }).compileToV0Message();
+
+    const transaction = new VersionedTransaction(messageV0);
+
+    transaction.sign([fromWallet]);
+
+    transactions.push(transaction);
+  }
+
+  const results = await Promise.all(
+    transactions.map(async (transaction, i) => {
+      const signature = await connection.sendRawTransaction(transaction.serialize());
+
+      console.log(`Sent transaction ${i} with signature: ${signature}`);
+
+      return connection.confirmTransaction(
+        {
+          signature,
+          lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+          blockhash: latestBlockhash.blockhash,
+        },
+        'confirmed',
+      );
+    }),
+  );
+
+  console.log('done', results);
 })();
---
+++ test-3.ts
@@ -0,0 +1,185 @@
+import Client, { CommitmentLevel, SubscribeRequest } from '@triton-one/yellowstone-grpc';
+import { createAssociatedTokenAccountIdempotentInstruction, getAssociatedTokenAddressSync } from '@solana/spl-token';
+import { LIQUIDITY_STATE_LAYOUT_V4, Liquidity, LiquidityPoolKeysV4, TokenAmount, WSOL } from '@raydium-io/raydium-sdk';
+import { ComputeBudgetProgram, Connection, PublicKey, TransactionMessage, VersionedTransaction } from '@solana/web3.js';
+import base58 from 'bs58';
+import {
+  COMMITMENT_LEVEL,
+  COMPUTE_UNIT_LIMIT,
+  COMPUTE_UNIT_PRICE,
+  createPoolKeys,
+  CUSTOM_FEE,
+  getMinimalMarketV3,
+  getToken,
+  getWallet,
+  GRPC_ENDPOINT,
+  GRPC_TOKEN,
+  NETWORK,
+  PRIVATE_KEY,
+  QUOTE_AMOUNT,
+  RPC_ENDPOINT,
+  RPC_WEBSOCKET_ENDPOINT,
+  TRANSACTION_EXECUTOR,
+} from './helpers';
+import { DefaultTransactionExecutor, TransactionExecutor } from './transactions';
+import { WarpTransactionExecutor } from './transactions/warp-transaction-executor';
+import { JitoTransactionExecutor } from './transactions/jito-rpc-transaction-executor';
+
+const snipeList = [''];
+
+(async function main() {
+  const connection = new Connection(RPC_ENDPOINT, {
+    // wsEndpoint: RPC_WEBSOCKET_ENDPOINT,
+    commitment: COMMITMENT_LEVEL,
+  });
+
+  const wallet = getWallet(PRIVATE_KEY.trim());
+  let quoteToken = getToken('WSOL');
+  const ataIn = getAssociatedTokenAddressSync(quoteToken.mint, wallet.publicKey);
+
+  let txExecutor: TransactionExecutor;
+
+  switch (TRANSACTION_EXECUTOR) {
+    case 'warp': {
+      txExecutor = new WarpTransactionExecutor(CUSTOM_FEE);
+      break;
+    }
+    case 'jito': {
+      txExecutor = new JitoTransactionExecutor(CUSTOM_FEE, connection);
+      break;
+    }
+    default: {
+      txExecutor = new DefaultTransactionExecutor(connection);
+      break;
+    }
+  }
+
+  const client = new Client(GRPC_ENDPOINT, GRPC_TOKEN, {});
+
+  const programId = '675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8';
+  const createPoolFeeAccount = '7YttLkHDoNj9wyDur5pM1ejNaAvT9X4eqaYcHQqtj2G5';
+  const rpcConnInfo = await client.subscribe();
+
+  rpcConnInfo.on('data', async (data: any) => {
+    if (!data.filters.includes('pool')) return undefined;
+
+    const info = data.transaction;
+    if (info.transaction.meta.err !== undefined) return undefined;
+
+    const accounts = info.transaction.transaction.message.accountKeys.map((i: Buffer) => base58.encode(i));
+
+    for (const item of [
+      ...info.transaction.transaction.message.instructions,
+      ...info.transaction.meta.innerInstructions.map((i: any) => i.instructions).flat(),
+    ]) {
+      if (accounts[item.programIdIndex] !== programId) continue;
+
+      if ([...(item.data as Buffer).values()][0] != 1) continue;
+
+      const keyIndex = [...(item.accounts as Buffer).values()];
+
+      let pairAccount = await connection.getAccountInfo(new PublicKey(accounts[keyIndex[4]]));
+      if (pairAccount === null) throw Error('get account info error');
+
+      let poolState = LIQUIDITY_STATE_LAYOUT_V4.decode(pairAccount!.data);
+
+      console.log({
+        baseMint: poolState.baseMint.toString(),
+        quoteMint: poolState.quoteMint.toString(),
+        time: +new Date(),
+      });
+
+      // if (poolState.status.toNumber() !== 6) continue;
+
+      const market = await getMinimalMarketV3(connection, new PublicKey(poolState.marketId), COMMITMENT_LEVEL);
+      const poolKeys: LiquidityPoolKeysV4 = createPoolKeys(new PublicKey(accounts[keyIndex[4]]), poolState, market);
+
+      let baseMint = poolState.quoteMint.equals(new PublicKey(WSOL.mint)) ? poolState.baseMint : poolState.quoteMint;
+
+      if (!snipeList.includes(baseMint.toString())) {
+        console.log(baseMint.toString(), 'Not in snipe list');
+        return;
+      }
+
+      const ataOut = getAssociatedTokenAddressSync(baseMint, wallet.publicKey);
+
+      const { innerTransaction } = Liquidity.makeSwapFixedInInstruction(
+        {
+          poolKeys: poolKeys,
+          userKeys: {
+            tokenAccountIn: ataIn,
+            tokenAccountOut: ataOut,
+            owner: wallet.publicKey,
+          },
+          amountIn: new TokenAmount(quoteToken, QUOTE_AMOUNT, false).raw,
+          minAmountOut: 0,
+        },
+        poolKeys.version,
+      );
+
+      const latestBlockhash = await connection.getLatestBlockhash();
+
+      const messageV0 = new TransactionMessage({
+        payerKey: wallet.publicKey,
+        recentBlockhash: latestBlockhash.blockhash,
+        instructions: [
+          ComputeBudgetProgram.setComputeUnitPrice({ microLamports: COMPUTE_UNIT_PRICE }),
+          ComputeBudgetProgram.setComputeUnitLimit({ units: COMPUTE_UNIT_LIMIT }),
+          createAssociatedTokenAccountIdempotentInstruction(wallet.publicKey, ataOut, wallet.publicKey, baseMint),
+          ...innerTransaction.instructions,
+        ],
+      }).compileToV0Message();
+
+      const transaction = new VersionedTransaction(messageV0);
+      transaction.sign([wallet, ...innerTransaction.signers]);
+
+      console.log('executeAndConfirm', +new Date());
+
+      const result = await txExecutor.executeAndConfirm(transaction, wallet, latestBlockhash);
+
+      if (result.confirmed) {
+        console.log(
+          {
+            time: +new Date(),
+            mint: baseMint.toString(),
+            signature: result.signature,
+            url: `https://solscan.io/tx/${result.signature}?cluster=${NETWORK}`,
+          },
+          `Confirmed buy tx,${+new Date()}`,
+        );
+      }
+    }
+  });
+
+  await new Promise<void>((resolve, reject) => {
+    if (rpcConnInfo === undefined) throw Error('rpc conn error');
+    rpcConnInfo.write(
+      {
+        slots: {},
+        accounts: {},
+        transactions: {
+          pool: {
+            accountInclude: [createPoolFeeAccount],
+            accountExclude: [],
+            accountRequired: [],
+          },
+        },
+        blocks: {},
+        blocksMeta: {},
+        accountsDataSlice: [],
+        entry: {},
+        commitment: 1,
+      },
+      (err: Error) => {
+        if (err === null || err === undefined) {
+          resolve();
+        } else {
+          reject(err);
+        }
+      },
+    );
+  }).catch((reason) => {
+    console.error(reason);
+    throw reason;
+  });
+})();
